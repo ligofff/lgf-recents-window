f1bdced37621d5ca93d5ea2c17fe25061c1ae352
ligofff <ligaofficial@yandex.ru>
2024-09-29 15:11:40 +0300

    Package Init

---
+++ Editor/RecentsHandler.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using Object = UnityEngine.Object;
+
+namespace Editor
+{
+    public static class RecentsHandler
+    {
+        public static int MaxRecents = 10;
+
+        private static string PrefsId = "RecentsWindowData";
+
+        private static List<string> IdsBuffer;
+        
+        public static event Action OnRecentsChanged;
+        
+        [InitializeOnLoadMethod]
+        public static void RecentsHandler_Init()
+        {
+            if (!EditorPrefs.HasKey(PrefsId))
+                EditorPrefs.SetString(PrefsId, "");
+            
+            PullRecentsBuffer();
+
+            Selection.selectionChanged += SelectionChanged;
+        }
+
+        private static void SelectionChanged()
+        {
+            AddObjectToRecents(Selection.activeObject);
+        }
+
+        public static void AddObjectToRecents(Object obj)
+        {
+            if (obj == null) return;
+            if (PrefabUtility.IsPartOfNonAssetPrefabInstance(obj)) return;
+            if (AssetDatabase.IsValidFolder(AssetDatabase.GetAssetPath(obj))) return;
+
+            var objectId = GlobalObjectId.GetGlobalObjectIdSlow(obj).ToString();
+            
+            PullRecentsBuffer();
+            
+            if (IdsBuffer.Contains(objectId))
+                IdsBuffer.Remove(objectId);
+
+            IdsBuffer.Insert(0, objectId);
+            IdsBuffer = IdsBuffer.Take(MaxRecents).ToList();
+            
+            PushRecentsBuffer();
+            
+            OnRecentsChanged?.Invoke();
+        }
+
+        private static void PushRecentsBuffer()
+        {
+            EditorPrefs.SetString(PrefsId, string.Join(";", IdsBuffer));
+        }
+
+        private static void PullRecentsBuffer()
+        {
+            IdsBuffer = EditorPrefs.GetString(PrefsId).Split(';').ToList();
+        }
+
+        public static List<Object> GetObjects()
+        {
+            var objects = new List<Object>();
+            foreach (var id in IdsBuffer)
+            {
+                if (GlobalObjectId.TryParse(id, out var globalObjectId))
+                {
+                    objects.Add(GlobalObjectId.GlobalObjectIdentifierToObjectSlow(globalObjectId));
+                }
+            }
+
+            return objects;
+        }
+    }
+}
---
+++ Editor/RecentsWindow.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using Sirenix.Utilities.Editor;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    public class RecentsWindow : EditorWindow
+    {
+        private List<Object> _objects;
+
+        [MenuItem("Window/Recents")]
+        public static void ShowWindow()
+        {
+            GetWindow<RecentsWindow>("Recents");
+        }
+
+        private void OnEnable()
+        {
+            _objects = new List<Object>();
+            RecentsHandler.OnRecentsChanged += OnRecentsChanged;
+            Repaint();
+        }
+
+        private void OnRecentsChanged()
+        {
+            _objects = RecentsHandler.GetObjects();
+            this.Repaint();
+        }
+
+        private void OnGUI()
+        {
+            var favoriteObjectGUIStyle = new GUIStyle(EditorStyles.objectField);
+            favoriteObjectGUIStyle.normal.textColor = Color.yellow;
+            
+            foreach (var obj in _objects)
+            {
+                if (obj == null) continue;
+
+                EditorGUILayout.BeginHorizontal();
+                
+                var controlRect = EditorGUILayout.GetControlRect();
+                var rect = controlRect;
+                rect.size = new Vector2(controlRect.size.y, controlRect.size.y);
+                if (GUI.Button(rect, "*"))
+                {
+                    
+                }
+
+                var rect2 = controlRect;
+                rect2.position += new Vector2(rect.size.x + 5, 0f);
+                rect2.size -= new Vector2(rect.size.x + 10, 0f);
+                /*
+                if (GUI.Button(rect2, new GUIContent(obj.name, AssetPreview.GetMiniTypeThumbnail(obj.GetType())),
+                        objectGUIStyle))
+                {
+                    EditorGUIUtility.PingObject(obj);
+                    Selection.activeObject = obj;
+                }
+                */
+
+                GUI.enabled = false;
+                EditorGUI.ObjectField(rect2, obj, typeof(Object), false);
+                GUI.enabled = true;
+
+
+                EditorGUILayout.EndHorizontal();
+            }
+        }
+    }
+}
