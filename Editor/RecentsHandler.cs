252feb1f3252cadb2425d3531465bb1d4fd847da
ligofff <ligaofficial@yandex.ru>
2024-09-30 11:00:31 +0300

    Favorites functionality added, clear recents button added

--- Editor/RecentsHandler.cs
+++ Editor/RecentsHandler.cs
@@ -8,19 +8,25 @@ namespace Editor
 {
     public static class RecentsHandler
     {
-        public static int MaxRecents = 10;
+        public static int MaxRecents = 20;
+        public static int MaxFavorites = 20;
 
-        private static string PrefsId = "RecentsWindowData";
+        private static string PrefsIdRecents = "RecentsWindowData";
+        private static string PrefsIdFavorites = "RecentsWindowData_Favorites";
 
         private static List<string> IdsBuffer;
+        private static List<string> IdsBuffer_Favorites;
         
-        public static event Action OnRecentsChanged;
+        public static event Action OnChanged;
         
         [InitializeOnLoadMethod]
         public static void RecentsHandler_Init()
         {
-            if (!EditorPrefs.HasKey(PrefsId))
-                EditorPrefs.SetString(PrefsId, "");
+            if (!EditorPrefs.HasKey(PrefsIdRecents))
+                EditorPrefs.SetString(PrefsIdRecents, "");
+            
+            if (!EditorPrefs.HasKey(PrefsIdFavorites))
+                EditorPrefs.SetString(PrefsIdFavorites, "");
             
             PullRecentsBuffer();
 
@@ -50,23 +56,89 @@ namespace Editor
             
             PushRecentsBuffer();
             
-            OnRecentsChanged?.Invoke();
+            OnChanged?.Invoke();
+        }
+
+        public static void AddObjectToFavorites(Object obj)
+        {
+            if (obj == null) return;
+            if (PrefabUtility.IsPartOfNonAssetPrefabInstance(obj)) return;
+            if (AssetDatabase.IsValidFolder(AssetDatabase.GetAssetPath(obj))) return;
+            
+            var objectId = GlobalObjectId.GetGlobalObjectIdSlow(obj).ToString();
+            
+            PullRecentsBuffer();
+            
+            if (IdsBuffer_Favorites.Contains(objectId))
+                IdsBuffer_Favorites.Remove(objectId);
+
+            IdsBuffer_Favorites.Insert(0, objectId);
+            IdsBuffer_Favorites = IdsBuffer_Favorites.Take(MaxFavorites).ToList();
+            
+            PushRecentsBuffer();
+            
+            OnChanged?.Invoke();
+        }
+        
+        public static void RemoveObjectFromFavorites(Object obj)
+        {
+            if (obj == null) return;
+            if (PrefabUtility.IsPartOfNonAssetPrefabInstance(obj)) return;
+            if (AssetDatabase.IsValidFolder(AssetDatabase.GetAssetPath(obj))) return;
+            
+            var objectId = GlobalObjectId.GetGlobalObjectIdSlow(obj).ToString();
+            
+            PullRecentsBuffer();
+            
+            if (IdsBuffer_Favorites.Contains(objectId))
+                IdsBuffer_Favorites.Remove(objectId);
+            
+            PushRecentsBuffer();
+            
+            OnChanged?.Invoke();
+        }
+
+        public static void ClearAllRecents()
+        {
+            PullRecentsBuffer();
+
+            IdsBuffer.Clear();
+            
+            PushRecentsBuffer();
+            
+            OnChanged?.Invoke();
         }
 
         private static void PushRecentsBuffer()
         {
-            EditorPrefs.SetString(PrefsId, string.Join(";", IdsBuffer));
+            EditorPrefs.SetString(PrefsIdRecents, string.Join(";", IdsBuffer));
+            EditorPrefs.SetString(PrefsIdFavorites, string.Join(";", IdsBuffer_Favorites));
         }
 
         private static void PullRecentsBuffer()
         {
-            IdsBuffer = EditorPrefs.GetString(PrefsId).Split(';').ToList();
+            IdsBuffer = EditorPrefs.GetString(PrefsIdRecents).Split(';').ToList();
+            IdsBuffer_Favorites = EditorPrefs.GetString(PrefsIdFavorites).Split(';').ToList();
         }
 
         public static List<Object> GetObjects()
         {
             var objects = new List<Object>();
             foreach (var id in IdsBuffer)
+            {
+                if (GlobalObjectId.TryParse(id, out var globalObjectId))
+                {
+                    objects.Add(GlobalObjectId.GlobalObjectIdentifierToObjectSlow(globalObjectId));
+                }
+            }
+
+            return objects;
+        }
+
+        public static List<Object> GetObjects_Favorites()
+        {
+            var objects = new List<Object>();
+            foreach (var id in IdsBuffer_Favorites)
             {
                 if (GlobalObjectId.TryParse(id, out var globalObjectId))
                 {
--- Editor/RecentsWindow.cs
+++ Editor/RecentsWindow.cs
@@ -1,5 +1,5 @@
 ﻿using System.Collections.Generic;
-using Sirenix.Utilities.Editor;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
@@ -7,7 +7,17 @@ namespace Editor
 {
     public class RecentsWindow : EditorWindow
     {
+        private class RecentObjectData
+        {
+            public Object obj;
+            public bool favorite;
+        }
+        
         private List<Object> _objects;
+        private List<Object> _objects_favorites;
+        private List<RecentObjectData> _sortedBuffer;
+
+        private GUIStyle _favoriteObjectGUIStyle;
 
         [MenuItem("Ligofff/Recents Window")]
         public static void ShowWindow()
@@ -18,55 +28,115 @@ namespace Editor
         private void OnEnable()
         {
             _objects = new List<Object>();
-            RecentsHandler.OnRecentsChanged += OnRecentsChanged;
-            _objects = RecentsHandler.GetObjects();
+            _objects_favorites = new List<Object>();
+            RecentsHandler.OnChanged += OnChanged;
+            OnChanged();
             Repaint();
         }
 
-        private void OnRecentsChanged()
+        private void OnChanged()
         {
             _objects = RecentsHandler.GetObjects();
+            _objects_favorites = RecentsHandler.GetObjects_Favorites();
+            _sortedBuffer = _objects
+                .Concat(_objects_favorites)
+                .Distinct()
+                .OrderByDescending(obj => _objects_favorites.Contains(obj))
+                .Select(obj => new RecentObjectData(){obj = obj, favorite = _objects_favorites.Contains(obj)})
+                .ToList();
             Repaint();
         }
 
         private void OnGUI()
         {
-            var favoriteObjectGUIStyle = new GUIStyle(EditorStyles.objectField);
-            favoriteObjectGUIStyle.normal.textColor = Color.yellow;
-            
-            foreach (var obj in _objects)
+            if (_favoriteObjectGUIStyle == null)
             {
-                if (obj == null) continue;
+                _favoriteObjectGUIStyle = new GUIStyle(EditorStyles.objectField);
+                _favoriteObjectGUIStyle.normal.textColor = Color.yellow;
+            }
+            
+            DrawHeader();
 
-                EditorGUILayout.BeginHorizontal();
+            if (_sortedBuffer.Count == 0)
+            {
+                EditorGUILayout.LabelField("No recents found.");
+            }
+            
+            foreach (var obj in _sortedBuffer)
+            {
+                DrawObject(obj);
+            }
+        }
+
+        private void DrawHeader()
+        {
+            var rect = EditorGUILayout.GetControlRect();
+            var buttonRect = rect;
+            buttonRect.size = new Vector2(120, buttonRect.size.y);
+            buttonRect.position += new Vector2(rect.size.x - 120, 0f);
+
+            if (GUI.Button(buttonRect, "Clear"))
+            {
+                RecentsHandler.ClearAllRecents();
+            }
+        }
+
+        private void DrawObject(RecentObjectData data)
+        {
+            if (data.obj == null) return;
+
+            EditorGUILayout.BeginHorizontal();
                 
-                var controlRect = EditorGUILayout.GetControlRect();
-                var rect = controlRect;
-                rect.size = new Vector2(controlRect.size.y, controlRect.size.y);
+            var controlRect = EditorGUILayout.GetControlRect();
+            var rect = controlRect;
+            rect.size = new Vector2(controlRect.size.y, controlRect.size.y);
+
+            if (data.favorite)
+            {
+                GUI.backgroundColor = Color.yellow;
                 if (GUI.Button(rect, "*"))
                 {
-                    
+                    RecentsHandler.RemoveObjectFromFavorites(data.obj);
                 }
-
-                var rect2 = controlRect;
-                rect2.position += new Vector2(rect.size.x + 5, 0f);
-                rect2.size -= new Vector2(rect.size.x + 10, 0f);
-                /*
-                if (GUI.Button(rect2, new GUIContent(obj.name, AssetPreview.GetMiniTypeThumbnail(obj.GetType())),
-                        objectGUIStyle))
-                {
-                    EditorGUIUtility.PingObject(obj);
-                    Selection.activeObject = obj;
-                }
-                */
-
-                GUI.enabled = false;
-                EditorGUI.ObjectField(rect2, obj, typeof(Object), false);
-                GUI.enabled = true;
-
-
-                EditorGUILayout.EndHorizontal();
+                GUI.backgroundColor = Color.white;
             }
+            else
+            {
+                if (GUI.Button(rect, "*"))
+                {
+                    RecentsHandler.AddObjectToFavorites(data.obj);
+                }
+            }
+
+            var rect2 = controlRect;
+            rect2.position += new Vector2(rect.size.x + 5, 0f);
+            rect2.size -= new Vector2(rect.size.x + 10, 0f);
+            /*
+            if (GUI.Button(rect2, new GUIContent(obj.name, AssetPreview.GetMiniTypeThumbnail(obj.GetType())),
+                    objectGUIStyle))
+            {
+                EditorGUIUtility.PingObject(obj);
+                Selection.activeObject = obj;
+            }
+            */
+
+            GUI.enabled = false;
+
+            if (data.favorite)
+            {
+                GUI.backgroundColor = Color.yellow;
+                EditorGUI.ObjectField(rect2, data.obj, typeof(Object), false);
+                GUI.backgroundColor = Color.white;
+            }
+            else
+            {
+                EditorGUI.ObjectField(rect2, data.obj, typeof(Object), false);
+            }
+            
+            GUI.enabled = true;
+
+
+            EditorGUILayout.EndHorizontal(); 
         }
     }
 }
